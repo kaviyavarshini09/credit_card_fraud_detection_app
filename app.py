e76b41c616c56f716cc3d00d8e20b0c4809e80e4
kaviyavarshini09 <kaviyavarshini129@gmail.com>
2025-06-22 23:30:01 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+model = pickle.load(open('xgb_model.pkl', 'rb'))
+
+st.title("💳 Credit Card Fraud Detection App")
+
+st.write("Enter 29 numbers (V1 to V28 and normAmount) separated by commas")
+
+user_input = st.text_input("Example: 0.1, -1.3, ..., 0.9")
+
+if st.button("Predict"):
+    try:
+        values = [float(i) for i in user_input.split(',')]
+        if len(values) != 29:
+            st.error("❗ Please enter exactly 29 values.")
+        else:
+            result = model.predict(np.array(values).reshape(1, -1))
+            label = "🚨 Fraud" if result[0] == 1 else "✅ Legit"
+            st.success(f"Prediction: {label}")
+    except:
+        st.error("❗ Invalid input format.")
